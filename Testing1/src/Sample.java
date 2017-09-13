a47716af45be82601fffa8e803be47c552efb682
ShyamVenkata.Prasad <ShyamVenkata.Prasad@Tri03ws-8s9DT12.trianz.int>
2017-09-13 13:42:28 +0530

    Test New c value commit

--- Testing1/src/Sample.java
+++ Testing1/src/Sample.java
@@ -4,6 +4,8 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Helo");
+int c=10;
+System.out.println("value of C is >>>>"+c);
 	}
 
 }
